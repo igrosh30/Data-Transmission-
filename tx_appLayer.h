bcee3d754a709b22c5ffe9db4c5f0fec6f1d62b6
AlexandreSaPt <86572238+AlexandreSaPt@users.noreply.github.com>
2026-04-03 12:47:52 +0100

    add igor changes

---
+++ tx_appLayer.h
@@ -0,0 +1,26 @@
+#ifndef TX_APPLAYER_H
+#define TX_APPLAYER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdint.h>   
+#include "tx_datalink.h"
+
+void sendFileSerialLink(int porta, const char *filename, 
+                        int baudRate, int nTries, int timeout);
+
+
+static int buildControlPacket(unsigned char controlByte, // Start: 2 End: 3 
+                              unsigned char *packet, 
+                              const char *filename, 
+                              long fileSize);
+
+static int buildDataPacket(unsigned char *packet, 
+                           const unsigned char *data, 
+                           int dataLen);
+
+
+#endif
